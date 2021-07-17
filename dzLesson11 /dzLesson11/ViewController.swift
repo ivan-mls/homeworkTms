167a822d4993366e4eb70ddf50ebc7f9a96874d3
ivan <ivn.mls96@gmail.com>
2021-07-18 02:25:32 +0300

    dz11Lesson

---
+++ dzLesson11 /dzLesson11/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  dzLesson11
+//
+//  Created by User on 16.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
+    @IBOutlet weak var view4: UIView!
+    @IBOutlet weak var view5: UIView!
+    @IBOutlet weak var view6: UIView!
+    @IBOutlet weak var view7: UIView!
+    @IBOutlet weak var view8: UIView!
+    @IBOutlet weak var view9: UIView!
+    @IBOutlet weak var view10: UIView!
+    @IBOutlet weak var view11: UIView!
+    @IBOutlet weak var view12: UIView!
+    @IBOutlet weak var view13: UIView!
+    @IBOutlet weak var view14: UIView!
+    @IBOutlet weak var view15: UIView!
+    @IBOutlet weak var view16: UIView!
+    @IBOutlet weak var view17: UIView!
+    @IBOutlet weak var view18: UIView!
+    @IBOutlet weak var view19: UIView!
+    @IBOutlet weak var view20: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view2.layer.cornerRadius = view2.frame.size.width / 2.1
+        view2.clipsToBounds = true
+        view3.layer.cornerRadius = view3.frame.size.width / 2.1
+        view3.clipsToBounds = true
+        view4.layer.cornerRadius = view4.frame.size.width / 2.1
+        view4.clipsToBounds = true
+        view5.layer.cornerRadius = view5.frame.size.width / 2.1
+        view5.clipsToBounds = true
+        view6.layer.cornerRadius = view6.frame.size.width / 2.1
+        view6.clipsToBounds = true
+        view7.layer.cornerRadius = view7.frame.size.width / 2.1
+        view7.clipsToBounds = true
+        view8.layer.cornerRadius = view8.frame.size.width / 2.1
+        view8.clipsToBounds = true
+        view9.layer.cornerRadius = view9.frame.size.width / 2.1
+        view9.clipsToBounds = true
+        view10.layer.cornerRadius = view10.frame.size.width / 2.1
+        view10.clipsToBounds = true
+        view11.layer.cornerRadius = view11.frame.size.width / 2.1
+        view11.clipsToBounds = true
+        view12.layer.cornerRadius = view12.frame.size.width / 2.1
+        view12.clipsToBounds = true
+        view13.layer.cornerRadius = view13.frame.size.width / 2.1
+        view13.clipsToBounds = true
+        view14.layer.cornerRadius = view14.frame.size.width / 2.1
+        view14.clipsToBounds = true
+        view15.layer.cornerRadius = view15.frame.size.width / 2.1
+        view15.clipsToBounds = true
+        view16.layer.cornerRadius = view16.frame.size.width / 2.1
+        view16.clipsToBounds = true
+        view17.layer.cornerRadius = view17.frame.size.width / 2.1
+        view17.clipsToBounds = true
+        view18.layer.cornerRadius = view18.frame.size.width / 4.5
+        view18.clipsToBounds = true
+        view19.layer.cornerRadius = view19.frame.size.width / 2.1
+        view19.clipsToBounds = true
+        view20.layer.cornerRadius = view20.frame.size.width / 2.1
+        view20.clipsToBounds = true
+       
+        
+        
+        
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
